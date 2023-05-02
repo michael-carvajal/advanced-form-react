e025be2a1c11854fbf655659ecbb2ee1b420c0e7
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-05-02 17:18:46 -0400

    forgot what i did

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,6 +1,16 @@
+import { CustomSensoryPreferences } from "./Context/SensoryPreferencesContext"
+
 const Report = () => {
+    const {visual, auditory, kinesthetic} = CustomSensoryPreferences()
     return (
-        <h2>Report</h2>
+        <div className="report">
+            <h2>Here are your results</h2>
+            <div className="results">
+                <div><p>Visual</p><p>{Math.floor(visual * 100) + "%" }</p></div>
+                <div><p>Auditory</p><p>{Math.floor(auditory * 100) + "%" }</p></div>
+                <div><p>Kinesthetic</p><p>{Math.floor(kinesthetic * 100) + "%" }</p></div>
+            </div>
+        </div>
     )
 }
 
